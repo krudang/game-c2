7271ed996d929354f0cf0ba6c8542525438c6ba8
krudang <42454814+krudang@users.noreply.github.com>
2019-09-14 07:29:19 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1568419468,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/set3_background-sheet0.png",
+		"images/set3_hills-sheet0.png",
+		"images/set3_tiles-sheet0.png",
+		"images/tiledbackground.png",
+		"images/stone-sheet0.png",
+		"images/stone-sheet1.png",
+		"images/flag-sheet0.png",
+		"images/player-sheet0.png",
+		"images/color.png",
+		"images/window-sheet0.png",
+		"images/gameover-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
